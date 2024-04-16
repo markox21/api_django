916d19c935b0d236ac618481ca222dc2ce07ca77
Marko Gamarra <markogamarra94@gmail.com>
2024-04-16 17:12:01 -0500

    api correo local orrait

--- projects/viewscorreo.py
+++ projects/viewscorreo.py
@@ -2,15 +2,11 @@ from django.http import JsonResponse
 
 def get_email_data(request):
     email_data = {
-        'subject': 'creo que ya esta',
-        'message': 'pene',
+        'subject': 'Recuperación de contraseña',
+        'message': '12345',
         'email_from': 'colladodeveloper123@gmail.com',
         'recipient_list': [
             'markogamarra94@gmail.com',
-            'oscarvasquezroncal@gmail.com',
-            'qryjeimpleis@gmail.com',
-            'leninalvaradob25@gmail.com',
-            'anivalmoraleshuanca@gmail.com'
         ]
     }
     return JsonResponse(email_data)
