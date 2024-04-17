a2731f131d84290901c66a22723c6b16a184a92c
Marko Gamarra <markogamarra94@gmail.com>
2024-04-17 08:38:03 -0500

    staticfiles

--- api_django/settings.py
+++ api_django/settings.py
@@ -143,7 +143,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
