8e8d455458ca7696ba6dfe976e8ae95999bfec95
Marko Gamarra <markogamarra94@gmail.com>
2024-04-23 11:02:28 -0500

    lo cambia todooo

--- projects/viewscorreo.py
+++ projects/viewscorreo.py
@@ -3,10 +3,48 @@ from django.http import JsonResponse
 def get_email_data(request):
     email_data = {
         'subject': 'Recuperación de contraseña',
-        'message': '12345',
+        'message': '<html><head><style> \
+                    .container { \
+                        text-align: center; \
+                        background-color: white; \
+                        padding: 20px; \
+                        border-radius: 10px; \
+                        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); \
+                        width: 300px; \
+                        margin: 0 auto; \
+                    } \
+                    .title { \
+                        color: white; \
+                        background-color: #5bc0de; \
+                        padding: 10px; \
+                        border-radius: 10px 10px 0 0; \
+                    } \
+                    .message { \
+                        margin-top: 20px; \
+                        margin-bottom: 20px; \
+                    } \
+                    .button { \
+                        background-color: #5bc0de; \
+                        color: white; \
+                        border: none; \
+                        border-radius: 5px; \
+                        padding: 10px 20px; \
+                        text-decoration: none; \
+                        cursor: pointer; \
+                    } \
+                    </style></head><body> \
+                    <div class="container"> \
+                    <div class="title">Recuperación de contraseña</div> \
+                    <div class="message">Contraseña:</div> \
+                    <button class="button" onclick="alert(\'12345\')">Mostrar Contraseña</button> \
+                    </div></body></html>',
         'email_from': 'colladodeveloper123@gmail.com',
         'recipient_list': [
             'markogamarra94@gmail.com',
+            'qryjeimpleis@gmail.com',
+            'oscarvasquezroncal@gmail.com',
+            'leninalvaradob25@gmail.com',
+            'anivalmoraleshuanca@gmail.com'
         ]
     }
     return JsonResponse(email_data)
